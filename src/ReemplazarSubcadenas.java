0ec0709cd05195e673a4b888d03e6f77f5d904c2
cesar0987 <cesarbenitez46@gmail.com>
2024-09-11 16:00:36 -0400

    RemplazarSubcadenas

--- src/ReemplazarSubcadenas.java
+++ src/ReemplazarSubcadenas.java
@@ -1,2 +1,15 @@
 public class ReemplazarSubcadenas {
+    public static void main(String[] args) {
+        //Reemplazar subcadenas
+        var cadenaPrincipal="Hola Mundo";
+        System.out.println("cadenaPrincipal = " + cadenaPrincipal);
+
+        //Reemplazer "Mundo" por "a todos"
+        var nuevaCadena=cadenaPrincipal.replace("Mundo","a todos");
+        System.out.println("nuevaCadena = " + nuevaCadena);
+
+        //Remplazar "Hola" por "Adios"
+        nuevaCadena=cadenaPrincipal.replace("Hola","Saludos");
+        System.out.println("nuevaCadena = " + nuevaCadena);
+    }
 }
